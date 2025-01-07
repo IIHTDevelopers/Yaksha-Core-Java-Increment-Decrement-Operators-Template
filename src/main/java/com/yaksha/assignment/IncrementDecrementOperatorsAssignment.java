17b280aba1316f7a695d8503846e79ed34ff8c05
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-01-07 14:40:44 +0530

    Update IncrementDecrementOperatorsAssignment.java

--- src/main/java/com/yaksha/assignment/IncrementDecrementOperatorsAssignment.java
+++ src/main/java/com/yaksha/assignment/IncrementDecrementOperatorsAssignment.java
@@ -4,13 +4,6 @@ public class IncrementDecrementOperatorsAssignment {
 
 	public static void main(String[] args) {
 
-		// Declare a variable
-		int count = 5;
-
-		// Perform increment and decrement operations
-		System.out.println("Pre-increment: " + (++count)); // Increments before printing
-		System.out.println("Post-increment: " + (count++)); // Prints first, then increments
-		System.out.println("Pre-decrement: " + (--count)); // Decrements before printing
-		System.out.println("Post-decrement: " + (count--)); // Prints first, then decrements
+		// Write your logic here
 	}
 }
